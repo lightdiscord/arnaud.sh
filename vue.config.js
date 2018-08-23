c27cf2dec6237d21a85e66d2d499ce9b1e3b6c50
LightDiscord <root@arnaud.sh>
2018-08-23 16:03:15 +0200

    manifest

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const path = require('path')
+
 const chainWebpack = config => {
     return config.module.rule('md')
         .test(/\.md$/)
@@ -9,6 +12,23 @@ const chainWebpack = config => {
         .options({ raw: true })
 }
 
+const manifest = new WebpackPwaManifest({
+    name: 'Arnaud.sh',
+    short_name: 'Arnaud.sh',
+    description: 'Home sweet home of LightDiscord',
+    background_color: '#000000',
+    theme_color: '#202124',
+    icons: [
+        {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512]
+        }
+    ]
+})
+
 module.exports = {
-    chainWebpack
+    chainWebpack,
+    configureWebpack: {
+        plugins: [ manifest ]
+    }
 }
