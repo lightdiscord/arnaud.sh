401aa778b091aa382ad1893e54ce4c0b6f7adf92
LightDiscord <root@arnaud.sh>
2020-03-25 01:17:35 +0100

    feat: delete everything

---
+++ README.md
@@ -0,0 +1,2 @@
+# arnaud.sh
+
--- src/store/mutations/store.js
+++
@@ -1,7 +0,0 @@
-export const storeRepositories = (state, repositories) => {
-    state.repositories = repositories
-}
-
-export const storeOrganizations = (state, organizations) => {
-    state.organizations = organizations
-}
