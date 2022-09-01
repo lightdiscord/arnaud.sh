bd5914ac92e576f4f6f9b704bd46b3704be32569
LightDiscord <root@arnaud.sh>
2022-09-01 16:14:46 +0200

    heading id and clickable

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,39 @@
 import RemarkHTML from "remark-html"
 import HtmlWebpackPlugin from "html-webpack-plugin"
 import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import { visit } from "unist-util-visit"
 
 import * as articles from "./articles/index.js"
+
+function remarkHeading() {
+  return (tree, file) => {
+    file.data.heading = []
+
+    visit(tree, "heading", (node) => {
+      const { depth, children: [child] } = node;
+      const { value } = child;
+
+      const id = value.toLowerCase()
+        .replaceAll(" ", "-")
+        .replace(/[^a-z0-9-]/g, "")
+
+      // Make the header clickable.
+      const link = {
+        type: "link",
+        url: `#${id}`,
+        children: [child]
+      };
+
+      node.children[0] = link;
+
+      // Add the correct id to the header
+      node.data = { hProperties: { id } };
+
+      // Register the header
+      file.data.heading.push({ depth, id, value })
+    })
+  }
+}
 
 export default (async () => ({
   mode: "development",
@@ -45,7 +76,11 @@ export default (async () => ({
             loader: "remark-loader",
             options: {
               remarkOptions: {
-                plugins: [RemarkHTML],
+                plugins: [
+                  remarkHeading,
+                  // Stop clobbering id.
+                  [RemarkHTML, { sanitize: { clobber: ["name"] } }]
+                ],
               },
             },
           },
