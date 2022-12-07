2d8c94c37fac77c39648b005181801559d3c6678
LightDiscord <root@arnaud.sh>
2022-12-07 20:43:35 +0100

    highlight and new note

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,10 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(syntaxHighlight)
+
     eleventyConfig.addPassthroughCopy("notes/**/*.png")
+    eleventyConfig.addPassthroughCopy({
+        "node_modules/prism-themes/themes/prism-vsc-dark-plus.css": "assets/css/prism-vsc-dark-plus.css",
+    })
 }
